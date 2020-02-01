2a4653674ecbbf4053c03597b7c2e4feb1eef4b4
elvengard <rifat.cse4.bu@gmail.com>
2020-02-01 14:10:07 +0600

    Bus Registration from admin

--- admin/addbus.php
+++ admin/addbus.php
@@ -8,46 +8,45 @@
                <div class="block copyblock"> 
 
 <?php
-    if($_SERVER['REQUEST_METHOD']=='POST'){
-    $C_Name=$fm->validation($_POST['C_Name']);
+if($_SERVER['REQUEST_METHOD']=='POST'){
+    $C_ID=$fm->validation($_POST['C_ID']);
     $Class_ID=$fm->validation($_POST['Class_ID']);
     $Category_ID=$fm->validation($_POST['Category_ID']);
-    $numrow=$fm->validation(md5($_POST['numrow']));
+    $numrow=$fm->validation($_POST['numrow']);
     $numcolleft=$fm->validation($_POST['numcolleft']);
     $numcolright=$fm->validation($_POST['numcolright']);
-    $lastrowseat=$fm->validation(md5($_POST['lastrowseat']));
+    $lastrowseat=$fm->validation($_POST['lastrowseat']);
+    $Coach_No=$fm->validation($_POST['Coach_No']);
+    $totalseats=(($numcolleft+$numcolright)*($numrow-1))+$lastrowseat;
 
 
-    $C_Name=mysqli_real_escape_string($db->link, $C_Name);
+    $C_ID=mysqli_real_escape_string($db->link, $C_ID);
     $Class_ID=mysqli_real_escape_string($db->link, $Class_ID);
     $Category_ID=mysqli_real_escape_string($db->link, $Category_ID);
     $numrow=mysqli_real_escape_string($db->link, $numrow);
     $numcolleft=mysqli_real_escape_string($db->link, $numcolleft);
     $numcolright=mysqli_real_escape_string($db->link, $numcolright);
     $lastrowseat=mysqli_real_escape_string($db->link, $lastrowseat);
-    
-    
+    $Coach_No=mysqli_real_escape_string($db->link, $Coach_No);
 
-    if(empty($C_Email) || empty($Password) || empty($C_Name) || empty($C_PhoneNo)){
-        echo "<span class='error'>Field Must Not Be Empty ! </span>";
+    
+    
+    $query = "select * from bus where C_ID = '$C_ID' and Coach_No = '$Coach_No'";
+    $result = $db->select($query);
+    if($result !=false){
+        echo "<span class='error'>Coach Already Registered!!</span><br>";
+        echo "<span class='error'>Multiple Coach With Same Coach No is Not Allowed For The Same Company!! </span>";
+    }else{
+        $query = "INSERT INTO bus (C_ID, Class_ID, Category_ID, Coach_No, totalseats, numrow, numcolleft, numcolright,lastrowseat) 
+        VALUES('$C_ID', '$Class_ID','$Category_ID','$Coach_No','$totalseats','$numrow','$numcolleft','$numcolright','$lastrowseat')";
+    $catinsert=$db->insert($query);
+    if($catinsert){
+        echo "<span class='success'>Coach Registered Successfully! </span>";
     }else {
-        $query1 = "select * from company where C_Email = '$C_Email'";
-        $row=$db->select($query1);
-        if($row){
-            echo "<span class='error'>Email Already Existed, Try Another Email! </span>";
-        }elseif(strlen($_POST['Password'])<6){
-            echo "<span class='error'>Password Must Be Atleast 6 Charecters !! </span>";
-        }else{
-            $query = "INSERT INTO company (C_Email, Password, C_PhoneNo, C_Name) VALUES('$C_Email', '$Password','$C_PhoneNo','$C_Name')";
-            $catinsert=$db->insert($query);
-            if($catinsert){
-                echo "<span class='success'>Company Created Successfully! </span>";
-            }else {
-                echo "<span class='error'>Company Not Created! </span>";
-            }
-        }
-        }
+        echo "<span class='error'>Coach Not Registered! </span>";
     }
+    }
+}
 ?>
 
                  <form action="" method="post">
@@ -57,12 +56,20 @@
                                 <label>Company</label>
                             </td>
                             <td>
-                                <select name="C_Name" class="medium">
-                                    <option value="volvo">Volvo</option>
-                                    <option value="saab">Saab</option>
-                                    <option value="fiat">Fiat</option>
-                                    <option value="audi">Audi</option>
-  </select>
+
+                                <select name="C_ID" class="medium" required>
+                                <option value="">Select From The List</option>
+                                <?php
+                                    $query="select * from company";
+                                    $blog_title=$db->select($query);
+                                    if($blog_title){
+                                    while($result = $blog_title->fetch_assoc()){
+                                ?>
+                                <option value=<?php echo $result['C_ID']; ?>><?php echo $result['C_Name']; ?>
+                                </option>
+                                <?php } } ?>
+                                </select>
+
                             </td>
                         </tr>					
                         <tr>
@@ -70,7 +77,18 @@
                                 <label>Class</label>
                             </td>
                             <td>
-                                <input type="text" name="Class_ID" placeholder="Class" class="medium" />
+                                <select name="Class_ID" class="medium" required>
+                                <option value="">Select From The List</option>
+                                <?php
+                                    $query="select * from class";
+                                    $blog_title=$db->select($query);
+                                    if($blog_title){
+                                    while($result = $blog_title->fetch_assoc()){
+                                ?>
+                                <option value=<?php echo $result['Class_ID']; ?>><?php echo $result['Class_Name']; ?>
+                                </option>
+                                <?php } } ?>
+                                </select>
                             </td>
                         </tr>
                         <tr>
@@ -78,7 +96,26 @@
                                 <label>Category</label>
                             </td>
                             <td>
-                                <input type="password" name="Category_ID" placeholder="Category" class="medium" />
+                                <select name="Category_ID" class="medium" required>
+                                <option value="">Select From The List</option>
+                                <?php
+                                    $query="select * from category";
+                                    $blog_title=$db->select($query);
+                                    if($blog_title){
+                                    while($result = $blog_title->fetch_assoc()){
+                                ?>
+                                <option value=<?php echo $result['Category_ID']; ?>><?php echo $result['Category_type']; ?>
+                                </option>
+                                <?php } } ?>
+                                </select>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <label>Coach No</label>
+                            </td>
+                            <td>
+                                <input type="number" name="Coach_No" placeholder="Coach No (Ex: 35)" class="medium" required/>
                             </td>
                         </tr>
                         <tr>
@@ -86,7 +123,7 @@
                                 <label>Number of Rows</label>
                             </td>
                             <td>
-                                <input type="text" name="numrow" placeholder="Number of Rows" class="medium" />
+                                <input type="number" name="numrow" placeholder="Number of Rows" class="medium" min="1" max="26" required/>
                             </td>
                         </tr>
                         <tr>
@@ -94,7 +131,7 @@
                                 <label>Number of Left Columns</label>
                             </td>
                             <td>
-                                <input type="text" name="numcolleft" placeholder="Num of Left Columns" class="medium" />
+                                <input type="number" name="numcolleft" placeholder="Num of Left Columns" class="medium" min="1" max="3" required/>
                             </td>
                         </tr>
                         <tr>
@@ -102,7 +139,7 @@
                                 <label>Number of Right Columns</label>
                             </td>
                             <td>
-                                <input type="text" name="numcolright" placeholder="Num of Right Columns" class="medium" />
+                                <input type="number" name="numcolright" placeholder="Num of Right Columns" class="medium" min="1" max="3" required/>
                             </td>
                         </tr>
                         <tr>
@@ -110,7 +147,7 @@
                                 <label>Number of Last row seats</label>
                             </td>
                             <td>
-                                <input type="text" name="lastrowseat" placeholder="Num of Last row seats" class="medium" />
+                                <input type="number" name="lastrowseat" placeholder="Num of Last row seats" class="medium" min="1" max="7" required/>
                             </td>
                         </tr>
 						<tr>
